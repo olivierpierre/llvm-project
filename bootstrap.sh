df8595cc753459b0284421d5e61e43e018f7937b
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2020-03-28 11:56:07 +0100

    add bootstrap script + update gitignore

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -rf build prefix &> /dev/null
+
+mkdir build
+mkdir prefix
+
+cd build
+cmake -G "Unix Makefiles" -DLLVM_ENABLE_PROJECTS=clang -DCMAKE_INSTALL_PREFIX=$PWD/../prefix -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_USE_LINKER=lld ../llvm
+make -j`nproc` install
+cd -
