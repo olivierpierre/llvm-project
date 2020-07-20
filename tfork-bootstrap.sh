dcfd61501954a8a5ab06d454b34221adb0f4fb32
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2020-07-20 10:17:20 +0200

    cleanup bootstrap script

---
+++ tfork-bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -rf build prefix &> /dev/null
+
+mkdir build
+mkdir prefix
+
+cd build
+cmake -G "Unix Makefiles" -DLLVM_ENABLE_PROJECTS=clang \
+    -DCMAKE_INSTALL_PREFIX=$PWD/../prefix -DLLVM_TARGETS_TO_BUILD="X86" \
+    -DLLVM_USE_LINKER=lld -DLLVM_ENABLE_RTTI=true -DLLVM_CCACHE_BUILD=true \
+    ../llvm
+make -j`nproc` install
+cd -
+
